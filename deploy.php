549f04ee93c1f6bd9adc5aa603a752414215173b
Mauro Morello <famiglia.morello@gmail.com>
2026-06-29 08:42:21 +0800

    forced full path to git pull command in deploy.php to avoid "git: command not found" error when running the script via cron job.

--- deploy.php
+++ deploy.php
@@ -5,7 +5,7 @@
 // Puoi aggiungere un token di sicurezza in seguito, per ora facciamo il meccanismo base.
 
 // 2. Esegui il comando di pull sulla cartella del server
-$output = shell_exec('cd /var/www/web/mdash && git pull 2>&1');
+$output = shell_exec('cd /var/www/web/mdash && /usr/bin/git pull 2>&1');
 
 // 3. Scrivi il risultato in un file di log per controllare se funziona
 file_put_contents('deploy_log.txt', date('[Y-m-d H:i:s] ') . $output, FILE_APPEND);
