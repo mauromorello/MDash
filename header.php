55083b70645e4c38be57ae87ca46f59d5cc07af9
Mauro Morello <famiglia.morello@gmail.com>
2026-07-13 09:23:32 +0800

    path

--- header.php
+++ header.php
@@ -21,21 +21,21 @@ if (is_array($pageHeadExtra)) {
     echo $pageHeadExtra . "\n";
 }
 ?>
-<link rel="apple-touch-icon" sizes="57x57" href="/MDash/icons/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/MDash/icons/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/MDash/icons/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/MDash/icons/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/MDash/icons/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/MDash/icons/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/MDash/icons/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/MDash/icons/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/MDash/icons/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="/MDash/icons/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/MDash/icons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="/MDash/icons/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/MDash/icons/favicon-16x16.png">
-<link rel="manifest" href="/MDash/icons/manifest.json">
+<link rel="apple-touch-icon" sizes="57x57" href="/mdash/icons/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/mdash/icons/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/mdash/icons/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/mdash/icons/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/mdash/icons/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/mdash/icons/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/mdash/icons/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/mdash/icons/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/mdash/icons/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/mdash/icons/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/mdash/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/mdash/icons/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/mdash/icons/favicon-16x16.png">
+<link rel="manifest" href="/mdash/icons/manifest.json">
 <meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/MDash/icons/ms-icon-144x144.png">
+<meta name="msapplication-TileImage" content="/mdash/icons/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
 </head>
