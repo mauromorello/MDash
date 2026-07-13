71f7f9324a846d216441db7b261155b5ae6d9633
Mauro Morello <famiglia.morello@gmail.com>
2026-07-13 09:13:49 +0800

    patch path

--- header.php
+++ header.php
@@ -21,21 +21,21 @@ if (is_array($pageHeadExtra)) {
     echo $pageHeadExtra . "\n";
 }
 ?>
-<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/manifest.json">
+<link rel="apple-touch-icon" sizes="57x57" href="/Mdash/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/Mdash/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/Mdash/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/Mdash/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/Mdash/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/Mdash/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/Mdash/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/Mdash/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/Mdash/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/Mdash/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/Mdash/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/Mdash/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/Mdash/favicon-16x16.png">
+<link rel="manifest" href="/Mdash/manifest.json">
 <meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="msapplication-TileImage" content="/Mdash/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
 </head>
